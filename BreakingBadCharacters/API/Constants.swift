ab0b993276828b8dfac10dd993990547b6947d88
khaoula.hafsia <khaoula.hafsia@proxym-it.com>
2021-02-02 14:26:16 +0100

    setting up project

---
+++ BreakingBadCharacters/API/Constants.swift
@@ -0,0 +1,46 @@
+//
+//  Constants.swift
+//  BreakingBadCharacters
+//
+//  Created by khaoula hafsia on 2/2/2021.
+//
+
+import Foundation
+
+  enum ApiError: Error {
+      case badRequest               //Status code 400
+      case notFound                //Status code 404
+      case unAuthorized           //Status code 401
+      case internalServerError   //Status code 500
+  }
+
+struct Constants {
+    //The API's base URL
+    static let baseURL = "hhttps://www.breakingbadapi.com/api/"
+    
+    //The parameters (Queries) that we're gonna use
+    struct Parameters {
+        static let episodes = "episodes "
+        static let name = "name"
+        static let character = "character"
+        static let category = "category"
+    }
+    
+    //The header fields
+    enum HttpHeaderField: String {
+        case authentication = "Authorization"
+        case contentType = "Content-Type"
+        case acceptType = "Accept"
+        case acceptEncoding = "Accept-Encoding"
+    }
+    
+    //The content type (JSON)
+    enum ContentType: String {
+        case json = "application/json"
+        case multipart = "multipart/form-data"
+        case text = "text/html; charset=utf-8"
+    }
+  
+}
+
+
---
+++ BreakingBadCharacters/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BreakingBadCharacters
+//
+//  Created by khaoula hafsia on 2/2/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
